d4dcea4318883dccdc15b832032ca818ef627502
RYUSEULGI <rhythm296@naver.com>
2021-05-12 16:48:17 +0900

    feat: crawl

---
+++ be/src/main/java/shop/goodcasting/api/common/crawl/CrawlController.java
@@ -0,0 +1,27 @@
+package shop.goodcasting.api.common.crawl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.goodcasting.api.article.hire.domain.Hire;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@Log
+@RequiredArgsConstructor
+@RequestMapping("/crawl")
+public class CrawlController {
+    private final CrawlServiceImpl service;
+
+    @GetMapping("/hire")
+    public ResponseEntity<List<Hire>> bugs(Hire hire) throws IOException {
+        log.info("hire접속");
+        log.info("hire" + hire);
+        return ResponseEntity.ok(service.saveAll(hire));
+    }
+}

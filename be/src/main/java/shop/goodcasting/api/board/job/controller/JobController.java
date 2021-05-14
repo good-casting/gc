4f3b21f98b2129cf9f40d8d30b5ff7f3117f9c3b
고동영 <kodongyoung@godong-yeong-ui-MacBookAir.local>
2021-05-14 14:27:44 +0900

    crud

--- be/src/main/java/shop/goodcasting/api/board/job/controller/JobController.java
+++ be/src/main/java/shop/goodcasting/api/board/job/controller/JobController.java
@@ -1,11 +1,44 @@
 package shop.goodcasting.api.board.job.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shop.goodcasting.api.board.job.domain.Job;
+import shop.goodcasting.api.board.job.domain.JobDTO;
 import shop.goodcasting.api.board.job.service.JobService;
+import shop.goodcasting.api.board.job.service.JobServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/job")
 public class JobController {
-    private final JobService service;
+
+    private final JobServiceImpl service;
+
+    @PostMapping("/save")
+    public ResponseEntity<Long> save(@RequestBody Job job){
+        return ResponseEntity.ok(service.save(job));
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Job>> findById(@PathVariable long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Job> update(@RequestBody JobDTO jobDTO) {
+        return ResponseEntity.ok(service.update(jobDTO));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Job id) {
+        return ResponseEntity.ok(service.delete(id));
+    }
 }
---
+++ be/src/main/java/shop/goodcasting/api/common/service/AbstractService.java
@@ -0,0 +1,16 @@
+package shop.goodcasting.api.common.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class AbstractService<T> {
+    public abstract long save(T t);
+    public abstract Optional<T> findById(long id);
+    public abstract List<T> findAll();
+    public abstract long count();
+    public abstract Optional<T> getOne(long id);
+    public abstract long delete(T t);
+    public abstract boolean existById(long id);
+
+}
